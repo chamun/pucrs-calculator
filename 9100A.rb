2712cf67dcb0fb1a859f9b295231644cf1c83a6c
Rodrigo Chamun <rochamun@gmail.com>
2015-09-10 12:11:10 -0300

    First commit
    Implements +, -, /, * operations

---
+++ 9100A.rb
@@ -0,0 +1,43 @@
+#!/usr/bin/env ruby
+
+class HP9100A
+  OPERATORS = %w(+ * - /)
+
+  def initialize(input)
+    @data = parse(input)
+  end
+
+  def self.calculate(input)
+    new(input).calculate
+  end
+
+  def calculate
+    operands = []
+    stack = []
+
+    while @data.size != 0
+      result = @data.shift
+      result = apply(result, stack) if operator?(result)
+      stack << result
+    end
+
+    stack.last
+  end
+
+  private
+
+  def operator?(operator)
+    OPERATORS.include?(operator)
+  end
+
+  def apply(operator, stack)
+    stack.pop(2).reverse.reduce(operator)  
+  end
+
+  def parse(data)
+    data.split(" ").map { |i| operator?(i) ? i : i.to_f }
+  end
+end
+
+input = '2 3 /'
+puts "Result is: #{ HP9100A.calculate(input) }"
